0b0f9379108ee8ce51c2bda295e05630c18a2bde
stux <hello@mstdn.social>
2022-09-02 23:31:52 +0200

    Contact temp

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -15,7 +15,7 @@
         <section class="py-12">
             <div class="max-w-7xl mx-auto px-6 lg:px-8">
 
-                <div class="columns-1 mb-6">
+                <!-- <div class="columns-1 mb-6">
 
                     <div class="card lg:card-side bg-base-300">
                         <figure><img src="https://placeimg.com/400/400/arch" alt="Album"></figure>
@@ -28,16 +28,14 @@
                         </div>
                     </div>
 
-                </div>
+                </div> -->
+
                 <div class="columns-1">
-                    Nunc malesuada dignissim sapien. Proin rhoncus bibendum gravida. Morbi nec risus augue. Ut mollis
-                    congue lectus vel feugiat. Fusce odio enim, facilisis porttitor ultrices sed, molestie vitae erat.
-                    Proin eget nisi felis. Morbi sit amet consectetur quam. Curabitur mattis sollicitudin ex vel
-                    fringilla. Nullam posuere euismod nisl. Integer at metus lectus.
+                    I am working on dissssss...!
                 </div>
 
 
-                <div class="columns-1 md:columns-2 lg:columns-2 mt-6">
+                <!-- <div class="columns-1 md:columns-2 lg:columns-2 mt-6">
                     <div class="card card-compact w-full bg-base-300">
                         <div class="card-body">
                             <h2 class="card-title">Shoes!</h2>
@@ -56,7 +54,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
             </div>
         </section>
